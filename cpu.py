d83378a0f07752cbebeb869db8e2980a915f7702
Mark Mossberg <mark.mossberg@gmail.com>
2015-05-02 21:36:32 -0400

    Add xor instr

--- cpu.py
+++ cpu.py
@@ -77,6 +77,12 @@ class CPU(object):
             rs = self.r.read(instr.ops[1])
             rt = int(instr.ops[2])
             self.r.write(rd, rs | rt)
+        elif instr.name == 'xor':
+            # xor rd, rs, rt
+            rd = instr.ops[0]
+            rs = self.r.read(instr.ops[1])
+            rt = self.r.read(instr.ops[2])
+            self.r.write(rd, rs ^ rt)
         elif instr.name == 'lw':
             # lw rt, offs(rs)
             rd = instr.ops[0]
