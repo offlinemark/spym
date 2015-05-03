f99581d5b47abf8d15cf08eff5afd47b64ccd05b
Mark Mossberg <mark.mossberg@gmail.com>
2015-05-03 11:03:43 -0400

    Improve Memory error checking

--- emu/memory.py
+++ emu/memory.py
@@ -4,13 +4,17 @@ class Memory(object):
         self.memory = bytearray([0] * size)
 
     def read(self, addr, count):
-        if addr + count > self.size:
-            raise Exception('read exceeds memory bounds')
+        if addr < 0 or addr + count > self.size:
+            raise Exception('read outside memory bounds')
+        elif count < 0:
+            raise Exception('negative memory read')
         return str(self.memory[addr:addr+count])
 
     def write(self, addr, buffer):
-        if addr + len(buffer) > self.size:
-            raise Exception('write exceeds memory bounds')
+        if addr < 0 or addr + len(buffer) > self.size:
+            raise Exception('write outside memory bounds')
+        elif len(buffer) == 0:
+            raise Exception('empty buffer to write')
         self.memory[addr:addr+len(buffer)] = buffer
 
     def dump(self):
