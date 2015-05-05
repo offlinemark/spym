7bbfc63cc8690b56329cf189911a02c6ab84774c
Mark Mossberg <mark.mossberg@gmail.com>
2015-05-05 00:31:46 -0400

    Make Memory size field a function

--- emu/memory.py
+++ emu/memory.py
@@ -2,22 +2,24 @@ class Memory(object):
     """Generic memory abstaction."""
 
     def __init__(self, size):
-        self.size = size
         self.memory = bytearray([0] * size)
 
     def read(self, addr, count):
-        if addr < 0 or addr + count > self.size:
+        if addr < 0 or addr + count > self.size():
             raise Exception('read outside memory bounds')
         elif count < 0:
             raise Exception('negative memory read')
         return str(self.memory[addr:addr+count])
 
     def write(self, addr, buffer):
-        if addr < 0 or addr + len(buffer) > self.size:
+        if addr < 0 or addr + len(buffer) > self.size():
             raise Exception('write outside memory bounds')
         elif len(buffer) == 0:
             raise Exception('empty buffer to write')
         self.memory[addr:addr+len(buffer)] = buffer
+
+    def size(self):
+        return len(self.memory)
 
     def dump(self):
         print repr(self.memory)
