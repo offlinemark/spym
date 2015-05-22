65845c645eb312755753ec7692f29e66207b11a2
Mark Mossberg <mark.mossberg@gmail.com>
2015-05-22 00:53:24 -0400

    Add SPYMHeader class to abstract dealing with headers, fix test

--- util/assemble.py
+++ util/assemble.py
@@ -42,6 +42,80 @@ def resolve(label):
 import parse
 
 
+class SPYMHeader(object):
+
+    def __init__(self, sections=None, binary=None):
+        """Construct header either from source (sections dict) or from binary
+        buffer. If both are given, it will go with sections.
+        """
+
+        self.magic = SPYM_MAGIC
+        # there's always text, and it's always right after the header
+        self.text_off = 28
+        self.text_size = 0
+        self.data_off = 0
+        self.data_size = 0
+        self.dtab_off = 0
+        self.dtab_size = 0
+
+        if sections:
+            self._from_sections(sections)
+        elif binary:
+            self._from_binary(binary)
+
+    def _from_sections(self, sections):
+        """Set header fields, given a dict of the schema
+        {'text': bytearray, 'data': bytearray, 'dtab': dict}.
+        """
+
+        offset = self.text_off
+        for section in sections:
+            size = len(sections[section])
+            if section == 'text':
+                self.text_size = size
+            elif section == 'data':
+                self.data_size = size
+                self.data_off = offset
+            elif section == 'dtab':
+                self.dtab_size = size
+                self.dtab_off = offset
+            else:
+                raise Exception('bad section')
+            offset += size
+
+    def _from_binary(self, binary):
+        hdr = struct.unpack('<4s6I', binary)
+        if hdr[0] != self.magic:
+            raise Exception('bad magic')
+        elif len(hdr) != 7:
+            raise Exception('bad header')
+
+        self.text_off = hdr[1]
+        self.text_size = hdr[2]
+        self.data_off = hdr[3]
+        self.data_size = hdr[4]
+        self.dtab_off = hdr[5]
+        self.dtab_size = hdr[6]
+
+    def to_binary(self):
+        return struct.pack('<4s6I', self.magic, self.text_off, self.text_size,
+                           self.data_off, self.data_size, self.dtab_off,
+                           self.dtab_size)
+
+    def dump(self):
+        print 'SPYM Header:'
+        self._dumpln('Magic', self.magic.encode('hex'))
+        self._dumpln('Text Start', self.text_off)
+        self._dumpln('Text Size', self.text_size)
+        self._dumpln('Data Start', self.data_off)
+        self._dumpln('Data Size', self.data_size)
+        self._dumpln('Data Table Start', self.dtab_off)
+        self._dumpln('Data Table Size', self.dtab_size)
+
+    def _dumpln(self, label, value):
+        print '  {}{}'.format((label + ':').ljust(20), value)
+
+
 def assemble(fname):
     """Generates a spym binary given a MIPS source filename.
 
@@ -58,23 +132,17 @@ def assemble(fname):
 
     with open(fname) as f:
         source = f.readlines()
-    header = bytearray(SPYM_MAGIC + struct.pack('<IIIIII', *[0]*6))
+    sections = {}
     body = ''
-    offset = len(header)
-    i = 4
-    sections = []
 
     dseg, tseg = parse.segments(source)
 
-    sections.append(parse.text_binary(tseg))
-    sections.append(parse.data(dseg) if dseg else dseg)
-    sections.append(pickle.dumps(datatab, pickle.HIGHEST_PROTOCOL))
+    sections['text'] = parse.text_binary(tseg)
+    sections['data'] = parse.data(dseg) if dseg else dseg
+    sections['dtab'] = pickle.dumps(datatab, pickle.HIGHEST_PROTOCOL)
 
-    for section in sections:
-        header[i:i+4] = struct.pack('<I', offset)
-        header[i+4:i+8] = struct.pack('<I', len(section))
-        offset += len(section)
-        body += section
-        i += 8
+    header = SPYMHeader(sections=sections)
+    for section in sections.keys():
+        body += sections[section]
 
-    return header + body
+    return header.to_binary() + body
