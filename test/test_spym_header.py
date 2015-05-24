d1175c8e205020c6a801050426fa68f6326a4b9b
Mark Mossberg <mark.mossberg@gmail.com>
2015-05-24 13:10:51 -0400

    Fix tests

--- test/test_spym_header.py
+++ test/test_spym_header.py
@@ -2,28 +2,23 @@ import pytest
 from util.assemble import SPYMHeader
 
 ideal_from_sections = SPYMHeader()
-ideal_from_sections.text_off = 28
+ideal_from_sections.text_off = 20
 ideal_from_sections.text_size = 4
-ideal_from_sections.data_off = 32
+ideal_from_sections.data_off = 24
 ideal_from_sections.data_size = 4
-ideal_from_sections.dtab_off = 36
-ideal_from_sections.dtab_size = 6
 
 
 def headers_are_same(one, two):
     return all([one.text_off == two.text_off,
                 one.text_size == two.text_size,
                 one.data_off == two.data_off,
-                one.data_size == two.data_size,
-                one.dtab_off == two.dtab_off,
-                one.dtab_size == two.dtab_size])
+                one.data_size == two.data_size])
 
 
 def test_spym_header():
     assert headers_are_same(ideal_from_sections,
-                            SPYMHeader({'text': 'text', 'data': 'data',
-                                       'dtab': 'sixsix'}))
+                            SPYMHeader({'text': 'text', 'data': 'data'}))
     with pytest.raises(Exception):
-        SPYMHeader(binary='lessthan28')
+        SPYMHeader(binary='lessthan20')
     with pytest.raises(Exception):
-        SPYMHeader(binary='a'*28)
+        SPYMHeader(binary='a'*20)
