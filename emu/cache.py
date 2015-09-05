1230e4cff22165055784cd8236297b59e478b69a
Mark Mossberg <mark.mossberg@gmail.com>
2015-09-05 13:57:53 -0700

    Begin cache write
    Begin implementing cache writes, currently tested. Ostensibly supports
    basic (non edge) cases like basic cache hits and misses. Does not
    handle accesses that straddle cache blocks

--- emu/cache.py
+++ emu/cache.py
@@ -1,5 +1,6 @@
-'''Direct-mapped data memory cache.
+'''Direct-mapped, write-back/allocate data memory cache.
 '''
+
 # ADDR_BITS = 32
 # CACHE_BYTES = 256
 # WORD_BYTES = 4
@@ -25,6 +26,7 @@ offset_mask = BLOCK_BYTES - 1
 class Block(object):
     def __init__(self):
         self.valid = False
+        self.dirty = False
         self.tag = 0
         self.data = bytearray([0] * BLOCK_BYTES)
 
@@ -44,6 +46,7 @@ class Cache(object):
 
     def read(self, addr):
         """Return word at addr, deferring to main mem if necessary."""
+        # TODO incorporate dirty flag where necessary
 
         caddr = CacheAddr(addr)
         block = self.cache[caddr.index]
@@ -63,6 +66,7 @@ class Cache(object):
                 return word
 
         # retrieve block from main memory
+        # TODO correct block alignment
         blk = self.dmem.read(addr, BLOCK_BYTES)
 
         # write back to cache
@@ -73,15 +77,46 @@ class Cache(object):
 
         return blk[:WORD_BYTES]
 
+    def write(self, addr, word):
+        '''Write word into addr, using write-back/allocate strategy.'''
 
-    def write(self, addr, buffer):
-        pass
+        assert len(word) == WORD_BYTES, \
+            'Cache write must be given word sized buffer'
+
+        caddr = CacheAddr(addr)
+        block = self.cache[caddr.index]
+
+        if block.valid and block.tag == caddr.tag:
+            # cache hit!
+
+            # redundant if this is the second write to this block
+            block.dirty = True
+
+            if caddr.offset + WORD_BYTES > BLOCK_BYTES:
+                # straddling blocks
+                # TODO
+                pass
+
+            block.data[caddr.offset:caddr.offset+WORD_BYTES] = word
+        else:
+            # cache miss
+            if block.valid and block.dirty:
+                # need to evict
+                self.dmem.write(addr, block.data)
+            # redundant if clean valid block
+            block.valid = True
+            block.dirty = True
+            block.tag = caddr.tag
+            block.data = self.dmem.read(addr, BLOCK_BYTES)
+            # TODO straddle
+            block.data[caddr.offset:caddr.offset+WORD_BYTES] = word
 
     def dump(self):
         log.info('Total Size: {} bytes'.format(CACHE_BYTES))
         log.info('Block Size: {} bytes'.format(BLOCK_BYTES))
         log.info('Blocks: {}\n'.format(num_blocks))
-        log.info('block\tvalid\ttag\tdata')
+        log.info('block\tvalid\tdirty\ttag\tdata')
         log.info('-'*40)
         for i, block in enumerate(self.cache):
-            log.info('{}\t{}\t{}\t{}'.format(i, block.valid, block.tag, binascii.hexlify(block.data)))
+            log.info('{}\t{}\t{}\t{}\t{}'.format(i, block.valid, block.dirty,
+                                                  block.tag, binascii.hexlify(block.data)))
